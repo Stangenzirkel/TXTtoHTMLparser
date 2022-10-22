713c22e57410fd90fcd1d893d1b773cadbc7ff36
Yuri Buyanov <yuri.buyanov@gmail.com>
2022-10-23 00:48:14 +0300

    add main function

--- main.py
+++ main.py
@@ -82,6 +82,10 @@ def index():
     return render_template("index.html", data=my_data)
 
 
-if __name__ == '__main__':
+def main():
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+
+if __name__ == '__main__':
+    main()
